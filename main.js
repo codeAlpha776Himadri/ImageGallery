ea7abf03c94f098ee93d2fc0fe1810dc726bee85
Himadri <hck364864@gmail.com>
2021-07-19 08:31:56 +0530

    Formatted

--- main.js
+++ main.js
@@ -1,18 +1,16 @@
 const panels = document.querySelectorAll('.panel')
 
-function toggleOpen()
-{
+function toggleOpen() {
     this.classList.toggle('open');
     this.childNodes[3].classList.toggle('addMiddleNote')
-    
-}
-panels.forEach(panel => panel.addEventListener('click' , toggleOpen))
 
-function showTexts(e){
+}
+panels.forEach(panel => panel.addEventListener('click', toggleOpen))
+
+function showTexts(e) {
     // headText.classList.toggle('.addHead')
     // lowerText.classList.toogle('.addFoot')
-    if(e.propertyName.includes('flex-grow'))
-    {
+    if (e.propertyName.includes('flex-grow')) {
         console.log(this.classList[2])
         const headText = document.querySelector(`.${this.classList[1]}`)
         headText.firstChild.nextSibling.classList.toggle('addHead')
@@ -22,9 +20,9 @@ function showTexts(e){
 
 
 
-panels.forEach(panel => panel.addEventListener('transitionend',showTexts))
+panels.forEach(panel => panel.addEventListener('transitionend', showTexts))
 
-document.querySelector('.btn').addEventListener('click' , () => {
+document.querySelector('.btn').addEventListener('click', () => {
     const screen = document.querySelector('.screen')
     screen.classList.add('screen-collapse')
 })
